93abbaada0c68c7d04fafbd3fc0745c456a0c4da
nstarman <nstarman@users.noreply.github.com>
2024-08-28 13:43:45 -0400

    feat: converters
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/dataclassish/__init__.py
+++ src/dataclassish/__init__.py
@@ -3,12 +3,14 @@
 dataclassish: dataclass tools, extended by multiple dispatch
 """
 
+from . import converters
 from ._core import DataclassInstance, F, asdict, astuple, fields, replace
 from ._ext import field_items, field_keys, field_values
 from ._version import version as __version__
 
 __all__ = [
     "__version__",
+    "converters",
     # core
     "DataclassInstance",
     "replace",
