0691e1100fc6a30a855ba74583d1da355c0aec8a
nstarman <nstarman@users.noreply.github.com>
2024-12-03 18:56:27 -0500

    🩹 fix-simple(cov): add pragmas

--- src/dataclassish/_src/api.py
+++ src/dataclassish/_src/api.py
@@ -25,25 +25,25 @@ from plum import dispatch
 @dispatch.abstract  # type: ignore[misc]
 def replace(obj: Any, /, **kwargs: Any) -> Any:
     """Replace the fields of an object."""
-    raise NotImplementedError
+    raise NotImplementedError  # pragma: no cover
 
 
 @dispatch.abstract  # type: ignore[misc]
 def fields(obj: Any, /) -> tuple[Field, ...]:  # type: ignore[type-arg]
     """Return the fields of an object."""
-    raise NotImplementedError
+    raise NotImplementedError  # pragma: no cover
 
 
 @dispatch.abstract  # type: ignore[misc]
 def asdict(obj: Any, /) -> dict[Hashable, Any]:
     """Return the fields of an object as a dictionary."""
-    raise NotImplementedError
+    raise NotImplementedError  # pragma: no cover
 
 
 @dispatch.abstract  # type: ignore[misc]
 def astuple(obj: Any, /) -> tuple[Any, ...]:
     """Return the fields of an object as a tuple."""
-    raise NotImplementedError
+    raise NotImplementedError  # pragma: no cover
 
 
 # ============================================================================
@@ -53,16 +53,16 @@ def astuple(obj: Any, /) -> tuple[Any, ...]:
 @dispatch.abstract  # type: ignore[misc]
 def field_keys(obj: Any, /) -> tuple[str, ...]:
     """Return the field names from the `dataclassish.fields`."""
-    raise NotImplementedError
+    raise NotImplementedError  # pragma: no cover
 
 
 @dispatch.abstract  # type: ignore[misc]
 def field_values(obj: Any, /) -> tuple[Any, ...]:
     """Return the field values from the `dataclassish.fields`."""
-    raise NotImplementedError
+    raise NotImplementedError  # pragma: no cover
 
 
 @dispatch.abstract  # type: ignore[misc]
 def field_items(obj: Any) -> tuple[tuple[str, Any], ...]:
     """Return the field items from the `dataclassish.fields`."""
-    raise NotImplementedError
+    raise NotImplementedError  # pragma: no cover
