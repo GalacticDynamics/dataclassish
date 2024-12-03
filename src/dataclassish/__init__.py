cd91062cc3a310c3bf7075b2a4271e45c972ad3e
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-12-03 18:45:31 -0500

    🎨 style(init): properly import some modules (#37)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/dataclassish/__init__.py
+++ src/dataclassish/__init__.py
@@ -3,7 +3,7 @@
 dataclassish: dataclass tools, extended by multiple dispatch
 """
 
-from . import converters
+from . import converters, flags
 from ._src.core import F, asdict, astuple, fields, replace
 from ._src.ext import field_items, field_keys, field_values
 from ._src.types import DataclassInstance
@@ -11,15 +11,16 @@ from ._version import version as __version__
 
 __all__ = [
     "__version__",
+    # Submodules
     "converters",
     "flags",
-    # core
+    # Dataclass API
     "DataclassInstance",
     "replace",
     "fields",
     "asdict",
     "astuple",
-    # ext
+    # Extensions
     "field_keys",
     "field_values",
     "field_items",
