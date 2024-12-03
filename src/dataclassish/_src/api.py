1391b2aa4762ac9198d3407828f26c3800826c33
nstarman <nstarman@users.noreply.github.com>
2024-12-03 18:28:13 -0500

    ✨ feat: add abstract dispatches

---
+++ src/dataclassish/_src/api.py
@@ -0,0 +1,68 @@
+"""API for functions in Dataclassish."""
+
+__all__ = [
+    # Dataclass API
+    "replace",
+    "fields",
+    "asdict",
+    "astuple",
+    # Extensions
+    "field_keys",
+    "field_values",
+    "field_items",
+]
+
+from collections.abc import Hashable
+from dataclasses import Field
+from typing import Any
+
+from plum import dispatch
+
+# ============================================================================
+# Dataclass API
+
+
+@dispatch.abstract  # type: ignore[misc]
+def replace(obj: Any, /, **kwargs: Any) -> Any:
+    """Replace the fields of an object."""
+    raise NotImplementedError
+
+
+@dispatch.abstract  # type: ignore[misc]
+def fields(obj: Any, /) -> tuple[Field, ...]:  # type: ignore[type-arg]
+    """Return the fields of an object."""
+    raise NotImplementedError
+
+
+@dispatch.abstract  # type: ignore[misc]
+def asdict(obj: Any, /) -> dict[Hashable, Any]:
+    """Return the fields of an object as a dictionary."""
+    raise NotImplementedError
+
+
+@dispatch.abstract  # type: ignore[misc]
+def astuple(obj: Any, /) -> tuple[Any, ...]:
+    """Return the fields of an object as a tuple."""
+    raise NotImplementedError
+
+
+# ============================================================================
+# Extensions
+
+
+@dispatch.abstract  # type: ignore[misc]
+def field_keys(obj: Any, /) -> tuple[str, ...]:
+    """Return the field names from the `dataclassish.fields`."""
+    raise NotImplementedError
+
+
+@dispatch.abstract  # type: ignore[misc]
+def field_values(obj: Any, /) -> tuple[Any, ...]:
+    """Return the field values from the `dataclassish.fields`."""
+    raise NotImplementedError
+
+
+@dispatch.abstract  # type: ignore[misc]
+def field_items(obj: Any) -> tuple[tuple[str, Any], ...]:
+    """Return the field items from the `dataclassish.fields`."""
+    raise NotImplementedError
