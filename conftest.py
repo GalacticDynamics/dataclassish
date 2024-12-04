63ecbc5796f54b0a31585ee4d34151fbb4415ebe
nstarman <nstarman@users.noreply.github.com>
2024-12-03 19:19:46 -0500

    🔥 dump(docs): remove unused docs and config

--- conftest.py
+++ conftest.py
@@ -1,15 +1,27 @@
 """Doctest configuration."""
 
+from collections.abc import Callable, Iterable, Sequence
 from doctest import ELLIPSIS, NORMALIZE_WHITESPACE
 
-from sybil import Sybil
-from sybil.parsers.rest import DocTestParser, PythonCodeBlockParser, SkipParser
+from sybil import Document, Region, Sybil
+from sybil.parsers.myst import (
+    DocTestDirectiveParser as MarkdownDocTestDirectiveParser,
+    PythonCodeBlockParser as MarkdownPythonCodeBlockParser,
+    SkipParser as MarkdownSkipParser,
+)
+from sybil.parsers.rest import DocTestParser as ReSTDocTestParser
 
-pytest_collect_file = Sybil(
-    parsers=[
-        DocTestParser(optionflags=NORMALIZE_WHITESPACE | ELLIPSIS),
-        PythonCodeBlockParser(),
-        SkipParser(),
-    ],
-    patterns=["*.rst", "*.py"],
-).pytest()
+optionflags = ELLIPSIS | NORMALIZE_WHITESPACE
+
+parsers: Sequence[Callable[[Document], Iterable[Region]]] = [
+    MarkdownDocTestDirectiveParser(optionflags=optionflags),
+    MarkdownPythonCodeBlockParser(doctest_optionflags=optionflags),
+    MarkdownSkipParser(),
+]
+
+docs = Sybil(parsers=parsers, patterns=["*.md"])
+python = Sybil(
+    parsers=[ReSTDocTestParser(optionflags=optionflags), *parsers], patterns=["*.py"]
+)
+
+pytest_collect_file = (docs + python).pytest()
